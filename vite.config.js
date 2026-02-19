6196e05b7c51c9a2609e3505f8df946f4de80f36
Dave <davekentorres@gmail.com>
2026-02-19 15:47:28 +0800

    Updated App.jsx layout > img location

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/",
   plugins: [react()],
-  base: "my-portfolio", // NOTE: set this to your repo name if you are deploying to GitHub Pages, otherwise you can remove this line
 })
